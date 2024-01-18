a46b989d81ca599b1e60e9e1005dd1c7e46a13ab
gajevski <mikolajxgajewski@gmail.com>
2024-01-18 21:56:13 +0100

    Initialize auth

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"squash-app-be/internal/auth"
 	"squash-app-be/internal/server"
 )
 
 func main() {
 
+	auth.NewAuth()
 	server := server.NewServer()
 
 	err := server.ListenAndServe()
