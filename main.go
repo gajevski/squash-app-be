179a0b088850a6285e40613a051d575d21001c56
gajevski <mikolajxgajewski@gmail.com>
2024-01-24 19:32:48 +0100

    Add login handler

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 
@@ -22,4 +25,35 @@ func main() {
 	r.HandleFunc("/login", loginHandler)
 	r.HandleFunc("/callback", callbackHandler)
 	http.ListenAndServe(":8080", r)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	url := oauth2Config.AuthCodeURL("state", oauth2.AccessTypeOffline)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func callbackHandler(w http.ResponseWriter, r *http.Request) {
+	code := r.URL.Query().Get("code")
+
+	token, err := oauth2Config.Exchange(context.Background(), code)
+	if err != nil {
+		http.Error(w, "Failed to exchange token: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	client := oauth2Config.Client(context.Background(), token)
+	userResponse, err := client.Get("https://api.github.com/user")
+	if err != nil {
+		http.Error(w, "Failed to get user info: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer userResponse.Body.Close()
+
+	var user map[string]interface{}
+	if err := json.NewDecoder(userResponse.Body).Decode(&user); err != nil {
+		http.Error(w, "Failed to decode user info: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "User Info: %v", user)
 }
